7b84b4f3799b7763c91f99cf03695a837982d5e9
dangerous3 <dangerous3@yandex.ru>
2020-02-04 17:35:19 +0300

    Решение задачи из второго степа модуля 2.1

---
+++ m2.1_working_with_excel_tables/task2.1-2.py
@@ -0,0 +1,11 @@
+import xlrd
+
+# Change your path to a file
+wb = xlrd.open_workbook('~/Загрузки/tab.xlsx')
+sheet_names = wb.sheet_names()
+sh = wb.sheet_by_name(sheet_names[0])
+nmin = sh.row_values(6)[2]
+for rownum in range(7, 27):
+    temp = sh.row_values(rownum)
+    nmin = min(nmin, temp[2])
+print(nmin)
