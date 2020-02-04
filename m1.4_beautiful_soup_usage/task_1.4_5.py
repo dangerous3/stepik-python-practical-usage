9d5d44133789c4d041f5781a791338285af1cc3f
dangerous3 <dangerous3@yandex.ru>
2020-02-04 16:18:05 +0300

    Решение задачи из пятого степа модуля 1.4

---
+++ m1.4_beautiful_soup_usage/task_1.4_5.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("https://stepik.org/media/attachments/lesson/209723/4.html")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+table_numbers_cells = soup.find_all('td')
+sum = 0
+
+for i in table_numbers_cells:
+    num_value = i.get_text()
+    sum = sum + int(num_value)
+
+print("Сумма равна: " + str(sum))
