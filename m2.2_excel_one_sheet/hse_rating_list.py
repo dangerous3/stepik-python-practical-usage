3f6746542694b38f24569d05896d458357cb3ca9
dangerous3 <dangerous3@yandex.ru>
2020-02-05 16:59:46 +0300

    Сделанное демо-задание из первого степа модуля 2.3

--- m2.2_excel_one_sheet/hse_rating_list.py
+++ m2.2_excel_one_sheet/hse_rating_list.py
@@ -1,5 +1,7 @@
 import xlrd, xlwt
 
+results = {}
+
 # Добавьте ваш путь к файлу
 rb = xlrd.open_workbook('~/Загрузки/hse_4zhur.xlsx')
 
@@ -9,13 +11,26 @@ sheet = rb.sheet_by_index(0)
 # получаем значение первой ячейки A1
 val = sheet.row_values(0)[0]
 
-print(val)
+#print(val)
 
 #получаем список значений из всех записей
 vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
+subjects = vals[9][17:-1]
 
 # выводим список предметов
-print(*vals[9][13:])
+print(*subjects)
+
+for i in range(11, len(vals)):
+    row = vals[i]
+    name = row[2]
+    grades = row[17:-1]
+    if name not in results:
+        results[name] = {}
+    for j in range(len(subjects)):
+        results[name][subjects[j] + '-4'] = grades[j]
+    # print(name, grades)
+    # print(row)
+print(results)
 
 # Выводим строки с информацией о первом студенте
-print(*vals[11])
+#print(*vals[11])
