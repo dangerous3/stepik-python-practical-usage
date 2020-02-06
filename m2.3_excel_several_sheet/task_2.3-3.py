2ca503f35a77c31ed1752c6c8c344cbf5dfae02d
dangerous3 <dangerous3@yandex.ru>
2020-02-06 21:48:13 +0300

    Финальное решение задачи степа 3 из модуля 2.3. Также в репозиторий загружен первоначальный вариант сэмпла к задаче.

--- m2.3_excel_several_sheet/task_2.3-3.py
+++ m2.3_excel_several_sheet/task_2.3-3.py
@@ -1,6 +1,7 @@
 import xlrd
 from collections import defaultdict
 import math
+import itertools
 
 rb = xlrd.open_workbook('~/Загрузки/trekking2.xlsx')
 sheet1 = rb.sheet_by_name('Справочник')
@@ -8,39 +9,41 @@ sheet2 = rb.sheet_by_name('Раскладка')
 
 values1 = [sheet1.row_values(rownum) for rownum in range(sheet1.nrows)]
 values2 = [sheet2.row_values(rownum) for rownum in range(sheet2.nrows)]
-#print(values)
 
 menu = defaultdict(list)
 reference = {}
-menu_item = []
-
 
 # Раскладка
 for cols in range(1,sheet2.nrows):
     menu[sheet2.cell_value(cols,0)].append(sheet2.cell_value(cols,1))
 print('Раскладка по меню на сегодня: {}'.format(menu))
 
-
 # Справочник
 for cols in range(1,sheet1.nrows):
     reference[sheet1.cell_value(cols,0)] = sheet1.row_values(cols,1)
-    # Меняем пустые строки на 0
+    # Меняем пустые ячейки из справочника на 0
     for i, sublist in enumerate(reference[sheet1.cell_value(cols,0)]):
         if sublist == '':
-            reference[sheet1.cell_value(cols, 0)][i] = 0
+            reference[sheet1.cell_value(cols, 0)][i] = float(0)
 print('Справочник продуктов: {}'.format(reference))
 
-
-
+# Функция подсчета суммы нутриентов
 def sum_of_nutrients(nutrient):
-    #component_num_column: 0 - calories, 1 - protein, 2 - fat, 3 - carbohydrate
+    '''nutrient option values:
+
+    0 - calories, 1 - protein, 2 - fat, 3 - carbohydrate'''
     result_list = []
     for key, value in menu.items():
-        result_list.append(list(map(lambda x: x / 100 * reference[key][nutrient], value)))
-    return [float(sum(j)) for j in zip(*result_list)]
+        result_list.append(list(map(lambda x: (x / 100) * reference[key][nutrient], value)))
+    # Возвращаем сумму нутриентов из сгенерированного одномерного списка
+    return(sum(list(itertools.chain.from_iterable(result_list))))
 
+# Вывод результатов с отбрасыванием дробной части
+print("-" * 20)
+print("Суммарная калорийность и сумма белков, жиров, углеводов по данной раскладке:")
 for i in range(4):
-    print(math.floor(*sum_of_nutrients(i)), end=" ")
+    print(math.floor(sum_of_nutrients(i)), end=" ")
+
 
 
 
