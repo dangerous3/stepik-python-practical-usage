07a803245216a7f540974ff232f4d635111c1c30
dangerous3 <dangerous3@yandex.ru>
2020-02-06 19:35:30 +0300

    Второе предварительное решение задачи 3 из модуля 2.3

--- m2.3_excel_several_sheet/task_2.3-3.py
+++ m2.3_excel_several_sheet/task_2.3-3.py
@@ -1,4 +1,6 @@
 import xlrd
+from collections import defaultdict
+import math
 
 rb = xlrd.open_workbook('~/Загрузки/trekking2.xlsx')
 sheet1 = rb.sheet_by_name('Справочник')
@@ -8,36 +10,37 @@ values1 = [sheet1.row_values(rownum) for rownum in range(sheet1.nrows)]
 values2 = [sheet2.row_values(rownum) for rownum in range(sheet2.nrows)]
 #print(values)
 
-menu = {}
+menu = defaultdict(list)
 reference = {}
+menu_item = []
 
-print(sheet2.nrows)
 
 # Раскладка
 for cols in range(1,sheet2.nrows):
-    menu[sheet2.cell_value(cols,0)] = sheet2.cell_value(cols,1)
-    print(sheet2.cell_value(cols,0), menu[sheet2.cell_value(cols,0)])
-print(menu)
+    menu[sheet2.cell_value(cols,0)].append(sheet2.cell_value(cols,1))
+print('Раскладка по меню на сегодня: {}'.format(menu))
+
 
 # Справочник
 for cols in range(1,sheet1.nrows):
     reference[sheet1.cell_value(cols,0)] = sheet1.row_values(cols,1)
+    # Меняем пустые строки на 0
+    for i, sublist in enumerate(reference[sheet1.cell_value(cols,0)]):
+        if sublist == '':
+            reference[sheet1.cell_value(cols, 0)][i] = 0
+print('Справочник продуктов: {}'.format(reference))
 
 
-#print(reference)
 
-calories = []
-protein = []
-fat = []
-carbohydrates = []
+def sum_of_nutrients(nutrient):
+    #component_num_column: 0 - calories, 1 - protein, 2 - fat, 3 - carbohydrate
+    result_list = []
+    for key, value in menu.items():
+        result_list.append(list(map(lambda x: x / 100 * reference[key][nutrient], value)))
+    return [float(sum(j)) for j in zip(*result_list)]
 
-for meal in menu.keys():
-    #print(meal)
-    calories.append(reference[meal][0])
-    protein.append(reference[meal][1])
-    fat.append(reference[meal][2])
-    carbohydrates.append(reference[meal][3])
-#print(calories, protein, fat, carbohydrates)
+for i in range(4):
+    print(math.floor(*sum_of_nutrients(i)), end=" ")
 
 
 
