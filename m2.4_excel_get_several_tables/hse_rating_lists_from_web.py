aca6e07ba5cd6d8db54dbc3416ec94d6ccfb102c
dangerous3 <dangerous3@yandex.ru>
2020-02-07 21:04:32 +0300

    Решение демо-задачи из степа 1 модуля 2.4 + сэмпл к заданию

---
+++ m2.4_excel_get_several_tables/hse_rating_lists_from_web.py
@@ -0,0 +1,61 @@
+import xlrd, xlwt
+import urllib.request
+
+results = {}
+files = [
+    ('1', 'https://www.hse.ru/data/2018/03/07/1165679773/%D0%A2%D0%B5%D0%BA%D1%83%D1%89%D0%B8%D0%B9%20%D1%80%D0%B5%D0%B9%D1%82%D0%B8%D0%BD%D0%B3%20%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%20%D0%BF%D0%B5%D1%80%D0%B5%D1%81%D0%B4%D0%B0%D1%87%201%20%D0%BA%D1%83%D1%80%D1%81%201%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80%202017-2018.xlsx'),
+    ('2', 'https://www.hse.ru/data/2018/03/07/1165680460/%D0%A2%D0%B5%D0%BA%D1%83%D1%89%D0%B8%D0%B9%20%D1%80%D0%B5%D0%B9%D1%82%D0%B8%D0%BD%D0%B3%20%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%20%D0%BF%D0%B5%D1%80%D0%B5%D1%81%D0%B4%D0%B0%D1%87%202%20%D0%BA%D1%83%D1%80%D1%81%201%20%D1%81%D0%B5%D0%BC%D0%B5%D1%81%D1%82%D1%80%202017-2018%2007.03.2018.xlsx'),
+    ('3', 'https://www.hse.ru/data/2018/03/07/1165666923/%D0%A2%D0%B5%D0%BA%D1%83%D1%89%D0%B8%D0%B9%20%D1%80%D0%B5%D0%B9%D1%82%D0%B8%D0%BD%D0%B3%20%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%20%D0%BF%D0%B5%D1%80%D0%B5%D1%81%D0%B4%D0%B0%D1%87%203%20%D0%BA%D1%83%D1%80%D1%81%201%20%D1%81%D0%B5%D0%BC%202017-2018.xlsx')
+]
+
+allsubj = set()
+
+for file in files:
+    course, url = file
+    urllib.request.urlretrieve(url, '../samples/testhse.xlsx')
+
+    # Добавьте ваш путь к файлу
+    rb = xlrd.open_workbook('~/Загрузки/hse_4zhur.xlsx')
+
+    # выбираем активный лист
+    sheet = rb.sheet_by_index(0)
+
+    # получаем значение первой ячейки A1
+    val = sheet.row_values(0)[0]
+
+    #print(val)
+
+    #получаем список значений из всех записей
+    vals = [sheet.row_values(rownum) for rownum in range(sheet.nrows)]
+    subjects = vals[9][17:-1]
+    for subj in subjects:
+        allsubj.add(subj + '-' + course)
+
+    # выводим список предметов
+    print(*subjects)
+
+    for i in range(11, len(vals)):
+        row = vals[i]
+        name = row[2]
+        grades = row[17:-1]
+        if name not in results:
+            results[name] = {}
+        for j in range(len(subjects)):
+            results[name][subjects[j] + '-' + course] = grades[j]
+        # print(name, grades)
+        # print(row)
+    print(results)
+allsubj = sorted(allsubj)
+print(' ', '\t'.join(allsubj), sep='\t')
+for name in results:
+    print(name, end='\t')
+    for subj in allsubj:
+        if subj in results[name]:
+            print(results[name][subj], end='\t')
+        else:
+            print('', end='\t')
+    print()
+
+
+    # Выводим строки с информацией о первом студенте
+    #print(*vals[11])
